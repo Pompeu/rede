9e06e8b48a27d22dc3ac6b670243ccf7171c7816
Pompeu <itacir@hotmail.com>
2015-03-11 17:19:02 -0300

    init stop neo4j on

--- initNeo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-clear
-sudo /opt/NEO4J_HOME/bin/neo4j start
-echo 'server On'
---
+++ stopneo4j.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+clear
+echo -e "552525\n" | sudo -S /opt/NEO4J_HOME/bin/neo4j stop
+echo 'server off'
