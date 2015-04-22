4230bbc46a0397ee55743c8b0120022a0784b8fa
Pompeu <itacir@hotmail.com>
2015-04-22 01:05:56 -0300

    rapair depencies and power-by express

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'app/public')));
+app.disable('x-powered-by');
 
 var jwtCheck = jwt({
   secret: 'redeapi'
