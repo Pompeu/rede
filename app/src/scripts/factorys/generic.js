73424b90cfa9b3a90b63bbffea39f8b2b2e793ab
Pompeu <itacir@hotmail.com>
2015-05-11 13:44:12 -0300

    factrys generic done

---
+++ app/src/scripts/factorys/generic.js
@@ -0,0 +1,39 @@
+(function(){
+  'use strict';
+  angular
+    .module('RedeApp')
+    .factory('generic', generic);
+
+    generic.$inject = ['$http'];
+
+    function generic($http) {
+
+      var service = {
+        get : get,
+        post : post,
+        put: put,
+        del : del
+      };
+
+      return service;
+
+      function get(resorce , id) {
+        if(id){
+          return $http.get('/api/'+resorce+'/'+id);
+        }        
+        return $http.get('/api/'+resorce);
+      }
+
+      function post(resorce, body) {
+        return $http.post('/api/'+resorce, body);
+      }
+
+      function put(resorce ,body) {
+        return $http.put('/api/'+resorce, body);
+      }
+
+      function del(resorce, id) {
+        return $http.del('/api/'+resorce+'/'+id);
+      }
+    }
+})();
