f4cdf3e6ad909dd62843d0806614d8929b66a12d
Pompeu <itacir@hotmail.com>
2015-07-31 15:32:19 -0300

    res.pipe done

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 var express = require('express'),
     router = express.Router(),
     path =  require('path'),
-    fs = require('fs');
+    fs = require('fs'),
+    stream,
+    mime = {'Content-Type': 'text/html'};
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  fs.createReadStream(path.join(__dirname, '../app/public/views/index.html'))
-    .pipe(res);
+  //res.set(mime);
+  stream.pipe(res);
 });
+
+(function(){    
+  stream = fs.createReadStream(path.join(__dirname, '../app/public/views/index.html'));
+}());
 
 module.exports = router;
