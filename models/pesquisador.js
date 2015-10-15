4f3fb75f2bedcffe7ccbf44a9272cc0e6c4c78a3
Pompeu <itacir@hotmail.com>
2015-10-15 09:57:48 -0300

    model pesquisador is strict and with requiments

--- models/pesquisador.js
+++ models/pesquisador.js
@@ -1,9 +1,9 @@
 // file: models/pesquisador.js - created at 2015-01-07, 06:44
-var model = require('seraph-model');
-
-var db = require('./neo');
-
-var Pesquisador = model(db,'Pesquisador');
+'use strict';
+const model = require(seraph-model);
+const db = require(./neo);
+const Pesquisador = model(db,Pesquisador);
+const validator = require('../plugins/index').validator;
 
 Pesquisador.fields = [
 		'nome',
@@ -19,5 +19,20 @@ Pesquisador.fields = [
 		'homePage',
 		'facebook'
 ];
+
+Pesquisador.schema = {
+		nome : { type : String, required: true},
+		telefoneProfissional : { type : String, match : validator.phone, required: true},
+		telefoneContato : { type : String,match : validator.phone, required: true},
+		emailProfissional : { type : String,match : validator.email, required: true},
+		formacaoNivel : { type : Date, required: true  },
+		formacaoAno : { type : Date, required: true},
+		enderecoProfissional : { type : String, required: true},
+		diretorioPesquisa : { type : String, required: true},
+		externo : { type : Boolean , required: true},
+		instituicao : { type : String, required: true},
+		homePage : { type : String, required: true},
+		facebook : { type : String, required: true}
+};
 
 module.exports = exports = Pesquisador;
