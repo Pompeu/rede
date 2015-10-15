8c1be9b5e05e2f6396cc490c7500e687d709ac3e
Pompeu <itacir@hotmail.com>
2015-10-15 11:18:16 -0300

    auth teste clean and same refactys

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -1,11 +1,22 @@
 // file: tests/auth.test.js - created at 2015-01-06, 04:04
+'use strict';
+
 var expect = require('chai').expect;
 var superagent = require('superagent');
 var url = require('url');
 var baseURL = 'http://localhost:3000/api/login';
+var jwt = require('jsonwebtoken');
+var getSecret =  require('../configs/apikey');
 
 describe('auth testing', function () {
 	
+	function testHelper (body, endHandler) {
+		superagent
+  		.post(url.resolve(baseURL,'login'))
+  		.send(body)
+  		.end(endHandler);
+	}	
+
   var body = {
 		email : 'itacir@hotmail.com',
 		password : '552525ia',
@@ -16,36 +27,31 @@ describe('auth testing', function () {
   	function endHandler(err, res) {
   		expect(err).to.be.null;
   		expect(res).to.exist;
-  		expect(res.body.result).to.an('object');
       expect(res.body.result.id_token).to.an('String');
       expect(res.body.status).to.be.ok;
-      expect(res.body.err).to.be.null;
+      expect(res.status).to.eql(200);
   		done();
   	}
-
-  	superagent
-  		.post(url.resolve(baseURL,'login'))
-  		.send(body)
-  		.end(endHandler);
+		testHelper(body,endHandler);	
   });
 
-  it('not retrive password in response of login' ,function (done) {
-      function endHandler(err, res) {
-      expect(err).to.be.null;
-      expect(res).to.exist;
-      expect(res.body.result.id_token).to.be.ok;
-      expect(res.body.result).to.an('object');
-      expect(res.body.result.password).to.be.undefined;
-      done();
-    }
+	it('should be a id_token cant have a property password', done => {
+		
+		function endHandler(err, res) {
+			let token = res.body.result.id_token;
+			let secret = getSecret.value;
+      expect(res.body.status).to.be.ok;
+      expect(res.body.err).to.be.null;
+		  jwt.verify(token, secret , (err , decoded) => {
+  			expect(decoded).to.not.have.property('password');
+				done()
+			});
+  	}
 
-    superagent
-      .post(url.resolve(baseURL,'login'))
-      .send(body)
-      .end(endHandler);
+		testHelper(body,endHandler);	
 
-  });
-
+	});
+  
   it('expect error if email or password is not ok', function (done) {
   	
     var login = {
@@ -56,14 +62,12 @@ describe('auth testing', function () {
   	function endHandler(err, res) {
   		expect(err).to.be.null;
   		expect(res).to.exist;
+  		expect(res.status).to.eql(401);
       expect(res.body.result.id_token).to.be.undefined;
   		expect(res.body.err).to.eql('login or passoword error');
   		done();
   	}
 
-  	superagent
-  		.post(url.resolve(baseURL,'login'))
-  		.send(login)
-  		.end(endHandler);
+		testHelper(login,endHandler);	
   });
 });
