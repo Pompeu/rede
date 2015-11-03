c0c4844be0ed41eb84ef72e56c8f6ae6166b960d
Pompeu <itacir@hotmail.com>
2015-11-03 17:25:38 -0200

    actualise package and gulfile

--- gulpfile.js
+++ gulpfile.js
@@ -49,20 +49,9 @@ gulp.task('scripts', function() {
 });
 
 gulp.task('scriptprod', function() {
-  return gulp.src([
-    'app/src/bower_components/angular/angular.min.js',
-		'app/src/bower_components/ui-router/release/angular-ui-router.min.js',
-    'app/src/bower_components/angular-animate/angular-animate.min.js',
-    'app/src/bower_components/angular-aria/angular-aria.min.js',
-    'app/src/bower_components/angular-material/angular-material.min.js',
-    'app/src//bower_components/a0-angular-storage/dist/angular-storage.min.js',
-    'app/src/bower_components/angular-jwt/dist/angular-jwt.min.js',
-    'app/src/scripts/app.js',
-    'app/src/scripts/factorys/*.js',    
-    'app/src/scripts/controllers/*.js',
-    'app/src/scripts/directives/**/*.js'])
-    .pipe(concat('all.min.js'))
+  return gulp.src('app/public/js/all.min.js')
     .pipe(uglify())
+    .pipe(concat('all.min.js'))
     .pipe(gulp.dest('app/public/js'))
 });
 
