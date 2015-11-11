626a9dc3f22f59801eda2dc3dd03dc34864e218b
Pompeu <itacir@hotmail.com>
2015-11-11 16:42:44 -0200

    refactor for 100% cover in getpubkey

--- plugins/getPugKey.js
+++ plugins/getPugKey.js
@@ -1,11 +1,10 @@
 // file: plugins/getPugKey.js - created at 2015-04-24, 03:51
 'use strict';
 const fs = require('fs');
-const pubKey = '/home/pompeu/.ssh/id_rsa.pub';
 
-function getPugKeyHandler(callback) {
+function getPugKeyHandler(path,callback) {
     
-  fs.readFile(pubKey, 'utf8', function(err , data) {
+  fs.readFile(path, 'utf8', function(err , data) {
     if(err) return callback(err);
     return callback(null,data.split(' ')[1]);
   });
--- tests/getPubkey.test.js
+++ tests/getPubkey.test.js
@@ -6,18 +6,29 @@ var getConfigSHKey = require('../configs/apikey');
 
 describe('getPubkey', function () {
   it('it should be open ssh pub key', function (done) {
-    
+    const pubKey = '/home/pompeu/.ssh/id_rsa.pub';
+
     function end(err, key) {
       key.should.be.an("string");
       key.should.have.length(372);
       done();
     }
-    getPubKey(end);
+
+    getPubKey(pubKey,end);
   });
 
   it('it shoud be open key ssh from config', function() {
     var key = getConfigSHKey;
     key.should.be.an("object");
     key.value.should.have.length(372);
+  });
+
+  it('should be get eror if key dont existe', () => {
+    const pubKey = "";
+    function end(err, data){
+      err.should.be.exist;
+      data.should.be.not.exist;
+    }
+    getPubKey(pubKey,end);
   });
 });
