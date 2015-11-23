a788ac981a445177eb0e81059e0e4329f7f8e487
Pompeu <itacir@hotmail.com>
2015-11-23 17:19:09 -0200

    controller of area projeto realtions ok

---
+++ controllers/projetodePesquisa/projetoGetAllRelArea.js
@@ -0,0 +1,7 @@
+// file: controllers/projetoGetAllRelArea.js - created at 2015-11-23, 02:56
+'use strict';
+function projetoGetAllRelAreaHandler(req, res) {
+  debug('get projetoGetAllRelArea handler controller');
+  res.json(res.locals.out);
+}
+module.exports = exports = projetoGetAllRelAreaHandler;
