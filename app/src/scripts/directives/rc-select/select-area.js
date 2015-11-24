94edef37c6504dbb98c72d8d403c80297339fda8
Pompeu <itacir@hotmail.com>
2015-11-24 16:59:10 -0200

    new directive rc-select

---
+++ app/src/scripts/directives/rc-select/select-area.js
@@ -0,0 +1,21 @@
+(function(){
+  'use strict';
+  angular.module('rc.select',[])
+    .directive('rcSelect', rcSelect);
+
+    function rcSelect () {
+      var directive = {
+        restrict : 'E',
+        scope : {
+          aria : "@",
+          item : "=",
+          items : "="
+        },
+        templateUrl : 'js/rc-select/select-area.html',
+      };
+
+      return directive;
+
+    }
+
+}());
